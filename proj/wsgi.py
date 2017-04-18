a5236c33dbf7caaa68c6e1cb518751774625a720
Alex Parsons <alex@alexparsons.co.uk>
2017-04-18 19:06:17 +0100

    temp fix

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
+sys.path.append("..\\inkleby_django\\")
+sys.path.append("../../useful_inkleby")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
