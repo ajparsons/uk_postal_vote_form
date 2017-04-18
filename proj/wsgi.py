158809f652aa4e9b942f458a87ebfd234bc40e28
Alex Parsons <alex@alexparsons.co.uk>
2017-04-18 19:09:39 +0100

    alt approach

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
-sys.path.append("..\\inkleby_django\\")
-sys.path.append("../../useful_inkleby")
+sys.path.append("../useful_inkleby")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
